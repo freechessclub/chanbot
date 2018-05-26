7a11d9de74b738a069800afe476341a8303c9987
The FreeChessClub Author(s) <support@freechess.club>
2018-05-26 00:34:33 -0700

    Remove hard-coded IP and use public/external ip

--- main.go
+++ main.go
@@ -15,6 +15,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"os"
+
 	"github.com/Sirupsen/logrus"
 )
 
@@ -23,9 +27,18 @@ var (
 )
 
 func main() {
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Stderr.WriteString("\n")
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	ip, err := ioutil.ReadAll(resp.Body)
 	user := "guest"
 	pass := ""
-	s, err := newSession(user, pass, "67.188.115.30")
+	s, err := newSession(user, pass, string(ip))
 	if err != nil {
 		log.WithField("err", err).Println("Failed to create a new session")
 		return
